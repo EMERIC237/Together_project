d71e92faa455421097dabea1817bd351278c2f1f
Emeric FOKOUONG TOUOLAC <bestvemeric@gmail.com>
2022-07-12 15:31:16 -0500

    working on home page

--- frontTo/src/App.js
+++ frontTo/src/App.js
@@ -3,17 +3,20 @@ import Navigation from "./Navigation";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Inscription from "./Inscription";
 import Members from "./Members";
+import "./app.css";
 
 function App() {
   return (
     <BrowserRouter>
       <Navigation />
-      <Routes className="page">
-        <Route path="/" element={<Home />} exact />
-        <Route path="/home" element={<Home />} exact />
-        <Route path="/members" element={<Members />} exact />
-        <Route path="/inscription" element={<Inscription />} exact />
-      </Routes>
+      <div className="pages">
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/home" element={<Home />} exact />
+          <Route path="/members" element={<Members />} exact />
+          <Route path="/inscription" element={<Inscription />} exact />
+        </Routes>
+      </div>
     </BrowserRouter>
   );
 }
--- frontTo/src/Navigation.jsx
+++ frontTo/src/Navigation.jsx
@@ -1,29 +1,45 @@
 import React, { Component } from "react";
 import { Navbar, Nav } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 import "./navigation.css";
 
 export class Navigation extends Component {
   render() {
     return (
-      <Navbar expand="lg" className="navStyle">
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Nav activeKey="/home">
-            <Nav.Item>
-              <Nav.Link href="/home">Home</Nav.Link>
-            </Nav.Item>
-            <Nav.Item>
-              <Nav.Link href="/inscription">Register</Nav.Link>
-            </Nav.Item>
-            {/*TODO: Remove this navigation if neccesary*/}
-            {/* <Nav.Item>
+      <Navbar expand="lg" className="navStyle" bg="info">
+        <Container>
+          <div>
+            <Navbar.Brand>
+              <div className="logoContainer">
+                <Image fluid rounded src="/images/Flag-Liberty.jpeg" />
+              </div>
+            </Navbar.Brand>
+          </div>
+          <div>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse
+              id="basic-navbar-nav"
+              className="justify-content-end"
+            >
+              <Nav activeKey="/home">
+                <Nav.Item>
+                  <Nav.Link href="/home">Home</Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link href="/inscription">Register</Nav.Link>
+                </Nav.Item>
+                {/*TODO: Remove this navigation if neccesary*/}
+                {/* <Nav.Item>
               <Nav.Link href="/login">Login</Nav.Link>
             </Nav.Item> */}
-            <Nav.Item>
-              <Nav.Link href="/members">Our Members</Nav.Link>
-            </Nav.Item>
-          </Nav>
-        </Navbar.Collapse>
+                <Nav.Item>
+                  <Nav.Link href="/members">Our Members</Nav.Link>
+                </Nav.Item>
+              </Nav>
+            </Navbar.Collapse>
+          </div>
+        </Container>
       </Navbar>
     );
   }
